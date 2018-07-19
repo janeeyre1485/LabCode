625f0f090a3458638b4533e7ff5632d2a9aef1f9
IoanaC <ioana.comanici@zoho.com>
2018-07-19 12:09:06 +0300

    added constructor

--- src/main/java/psi/model/StaticItem.java
+++ src/main/java/psi/model/StaticItem.java
@@ -1,7 +1,13 @@
 package psi.model;
 
 public class StaticItem implements Item {
+	
 	private  String value;
+
+	public StaticItem(String value) {
+		super();
+		this.value = value;
+	}
 
 	public String getValue() {
 		return value;
